41f1e8b9a9fd83f3d0291cd8965f9c7753e64463
qaisaru@icloud.com <qaisaru@icloud.com>
2018-09-10 08:15:00 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,10 +1,13 @@
 # Write a speak_to_grandma method.
-
+def speak_to_grandma(phrase)
+ if phrase = "I LOVE YOU GRANDMA!"
+   return "I LOVE YOU TOO PUMPKIN!"
+end
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
 
-# If you shout, she can hear you (or at least she thinks so) 
+# If you shout, she can hear you (or at least she thinks so)
 # and yells back
 
 # NO, NOT SINCE 1938!
