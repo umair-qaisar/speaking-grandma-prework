3f5f5b225a13116af877af0cecceef60ec4afa91
qaisaru@icloud.com <qaisaru@icloud.com>
2018-09-10 08:16:50 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -1,6 +1,6 @@
 # Write a speak_to_grandma method.
 def speak_to_grandma(phrase)
- if phrase = "I LOVE YOU GRANDMA!"
+ if phrase == "I LOVE YOU GRANDMA!"
    return "I LOVE YOU TOO PUMPKIN!"
 end
 # Whatever you say to grandma, she should respond with
